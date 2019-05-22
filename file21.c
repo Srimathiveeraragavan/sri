e94b4e833eece3f6de8c29a633615d9e89b277e3
Srimathiveeraragavan <50795662+Srimathiveeraragavan@users.noreply.github.com>
2019-05-22 10:53:06 +0530

    Update file21.c

--- file21.c
+++ file21.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 main()
 {
-  int n,a,d,i,r=0
+  int n,a,d,i,r=0;
   scanf("%d %d %d",&n,&a,&d);
   for(i=0;i<n;++i){
-  d=d+a;
-  r +=d;
   
+  r +=d;
+  d=d+a;
   }
-  printf(" %d",r);
+  printf("%d",r);
 }
