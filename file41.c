2b1b3f08fc84921a5564d353271767599645e098
Srimathiveeraragavan <50795662+Srimathiveeraragavan@users.noreply.github.com>
2019-05-23 12:56:05 +0530

    Create file41.c

---
+++ file41.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+main() {
+	// your code goes here
+	int n,i,j=0;
+	char s[100];
+	scanf("%s",&s);
+//	printf("%s",s);
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+	if(j==0){
+printf("%s",s);
+j=3;}
+else{
+	printf("\n%s",s);}
+}
+}
