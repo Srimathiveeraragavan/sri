bf9f97372f907494dbf6ebcba133a20d36fc1370
Srimathiveeraragavan <50795662+Srimathiveeraragavan@users.noreply.github.com>
2019-06-28 10:36:20 +0530

    Update register.php

--- blog/register.php
+++ blog/register.php
@@ -1,1 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>signin</title>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="sri.css">
+</head>
+
+<body>
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+	
+	<div class="a" >
+	<div class="container">
+	<h1></h1>
+	</div>
+	</div>
+	
+<div class="container" <!--style="background: linear-gradient(to bottom, #006699 0%, #003366 100%);--></div> ">
+<div class="row">
+	
+	<div class="col-sm-12">
+		<div class="col-sm-12">
+			<form action="register1.php" method="post"><br>
+				Enter email addrerss  : <input type="text" name="email"><br><br>
+				Enter your password : <input type="password" name="password"><br><br>
+				selcect your Access mode  :<select name="access">
+							<option value="admin" name="admin">admin</option>
+							<option value="user" name="user">user</option>
+							</select><br><br>
+				<input type="SUBMIT" VALUE="login"><br>
+			</form>
+		</div>
+	</div>
+	
+</div>
+</div>
+
+</body>
+
+</html>
 
